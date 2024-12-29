123b0fbf43ff99bd71628bd96ad5540e9a9ca088
TejasDesai197 <tejasdesai97@outlook.com>
2024-12-29 23:50:53 +0530

    commit message

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using MyFirstWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace MyFirstWebApplication.Controllers
+{   
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("About")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
